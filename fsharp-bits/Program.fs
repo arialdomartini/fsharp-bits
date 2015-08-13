97e9b4f76caee04956c4f87b65ee64f325b3239a
Arialdo Martini <arialdomartini@gmail.com>
2015-08-13 23:30:40 +0200

    Defining a simple inline function

--- fsharp-bits/Program.fs
+++ fsharp-bits/Program.fs
@@ -1,4 +1,7 @@
-﻿[<EntryPoint>]
+﻿let square x = x * x
+
+[<EntryPoint>]
 let main argv = 
     [1..100] |> List.sum |> printfn "The sum is=%d"
+    square 100 |> printfn "The square or 100 is %d"
     0
