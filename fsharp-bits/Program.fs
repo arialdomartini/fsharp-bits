a86a1b3932bc4ea786b2ba647b534a80be7a53aa
Arialdo Martini <arialdomartini@gmail.com>
2015-08-13 23:34:38 +0200

    Crating a type and using type inference

--- fsharp-bits/Program.fs
+++ fsharp-bits/Program.fs
@@ -1,7 +1,12 @@
 ﻿let square x = x * x
 
+type Person = { First:string; Last:string; }
+
 [<EntryPoint>]
 let main argv = 
     [1..100] |> List.sum |> printfn "The sum is=%d"
     square 100 |> printfn "The square or 100 is %d"
+
+    let mario = {First="Mario"; Last="Rossi"}
+    mario.Last |> printfn "His lasst name is %s"
     0
