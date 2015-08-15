6ea72181eb468ac7ca374488706af3fc0c8aaba4
Arialdo Martini <arialdomartini@gmail.com>
2015-08-16 00:15:16 +0200

    Calculating the product of all the numbers up to N

---
+++ fsharp-bits/CommonFunctionalities.fsx
@@ -0,0 +1,8 @@
+﻿// Calculating the product of all the numbers up to N
+
+let product a b = a * b
+
+let productOfNumbersUpTo n =
+    [1..n] |> List.fold product 1
+
+printfn "%d" (productOfNumbersUpTo 10)
