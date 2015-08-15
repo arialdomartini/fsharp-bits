64a1c36476c4b05dc804379aa1390c726fa8c89c
Arialdo Martini <arialdomartini@gmail.com>
2015-08-16 00:46:23 +0200

    Using a type rather than a tuple

--- fsharp-bits/CommonFunctionalities.fsx
+++ fsharp-bits/CommonFunctionalities.fsx
@@ -1,16 +1,14 @@
 ﻿// Calculating the product of all the numbers up to N
 
-//type Fold = {funct:(int-> int -> int) ; initialValue:int }
-
+type Fold = {funct:int-> int -> int ; initialValue:int }
 
 let filterAndFold n filter foldFunction =
-    let funct, initialValue = foldFunction
     [1..n] 
     |> List.filter filter 
-    |> List.fold funct initialValue
+    |> List.fold foldFunction.funct foldFunction.initialValue
 
 let product a b = a * b
-let productFold = (product,  1)
+let productFold = { funct=product; initialValue= 1}
 let all n = true
 let productOfNumbersUpTo n =
     filterAndFold n all productFold
@@ -21,7 +19,7 @@ printfn "%d" (productOfNumbersUpTo 10)
 // Counting the sum of odd numbers up to N
 
 let sum a b = a + b
-let sumFold = (sum, 0)
+let sumFold = {funct=sum; initialValue=0}
 let isOdd n = not (n % 2 = 0)
 
 let sumOfNumbersUpTo n =
