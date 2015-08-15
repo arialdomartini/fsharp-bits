d0bc56d0f61d3f057a3dd3864a86e3df744f2df9
Arialdo Martini <arialdomartini@gmail.com>
2015-08-16 00:16:42 +0200

    Counting the sum of odd numbers up to N

--- fsharp-bits/CommonFunctionalities.fsx
+++ fsharp-bits/CommonFunctionalities.fsx
@@ -6,3 +6,14 @@ let productOfNumbersUpTo n =
     [1..n] |> List.fold product 1
 
 printfn "%d" (productOfNumbersUpTo 10)
+
+
+// Counting the sum of odd numbers up to N
+
+let sum a b = a + b
+let isOdd n = not (n % 2 = 0)
+
+let sumOfNumbersUpTo n =
+    [1..n] |> List.filter isOdd |> List.fold sum 0
+
+printfn "%d" (sumOfNumbersUpTo 10)
