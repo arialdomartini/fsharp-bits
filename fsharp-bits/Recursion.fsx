eb7eba4caef623928dafbacaf6d25df4dc7b1cea
Arialdo Martini <arialdomartini@gmail.com>
2015-08-15 22:59:04 +0200

    A simple tail recursion

---
+++ fsharp-bits/Recursion.fsx
@@ -0,0 +1,8 @@
+﻿let incrementBy1 i = i + 1
+
+let rec visitList list operation =
+    match list with
+        | [] -> []
+        | first::rest -> (first + 1) :: (visitList rest operation)
+
+printfn "result: %A" (visitList [1..10] incrementBy1)
