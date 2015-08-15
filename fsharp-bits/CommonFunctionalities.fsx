570dee34e68625d310b78a1a992bfaad7865b096
Arialdo Martini <arialdomartini@gmail.com>
2015-08-16 00:39:09 +0200

    Shared functinalities

--- fsharp-bits/CommonFunctionalities.fsx
+++ fsharp-bits/CommonFunctionalities.fsx
@@ -1,9 +1,19 @@
 ﻿// Calculating the product of all the numbers up to N
 
-let product a b = a * b
+//type Fold = {funct:(int-> int -> int) ; initialValue:int }
 
+
+let filterAndFold n filter foldFunction =
+    let funct, initialValue = foldFunction
+    [1..n] 
+    |> List.filter filter 
+    |> List.fold funct initialValue
+
+let product a b = a * b
+let productFold = (product,  1)
+let all n = true
 let productOfNumbersUpTo n =
-    [1..n] |> List.fold product 1
+    filterAndFold n all productFold
 
 printfn "%d" (productOfNumbersUpTo 10)
 
@@ -11,9 +21,10 @@ printfn "%d" (productOfNumbersUpTo 10)
 // Counting the sum of odd numbers up to N
 
 let sum a b = a + b
+let sumFold = (sum, 0)
 let isOdd n = not (n % 2 = 0)
 
 let sumOfNumbersUpTo n =
-    [1..n] |> List.filter isOdd |> List.fold sum 0
+    filterAndFold n isOdd sumFold
 
 printfn "%d" (sumOfNumbersUpTo 10)
