0a834c355e4453c96d2b39cec7953cd095ad7b01
Arialdo Martini <arialdomartini@gmail.com>
2015-08-17 17:11:20 +0200

    3 simple arithmetic functions

---
+++ fsharp-bits/FunctionsAsBuildingBlocks.fsx
@@ -0,0 +1,7 @@
+﻿let add2 x = x + 2
+let multiplicateBy2 x = x * 2
+let square x = x * x
+
+[1..10] |> List.map  add2 |> printfn "%A"
+[1..10] |> List.map  multiplicateBy2 |> printfn "%A"
+[1..10] |> List.map  square |> printfn "%A"
