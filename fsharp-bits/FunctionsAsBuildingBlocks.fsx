28316954c05de5a3e8d37915d477f1202fa280f5
Arialdo Martini <arialdomartini@gmail.com>
2015-08-17 17:48:15 +0200

    Playning with high order functions and the composition operator

--- fsharp-bits/FunctionsAsBuildingBlocks.fsx
+++ fsharp-bits/FunctionsAsBuildingBlocks.fsx
@@ -17,3 +17,26 @@ let square x = x * x
 
 let add2ThenMultBy3 = add2 >> multBy3
 add2ThenMultBy3 8 |> printfn "8 2 + 3 * ->%A"
+
+
+let makeLog (funct:Printf.TextWriterFormat<'T> -> 'T) =
+    let log message value =
+        funct "%s%i; " message value
+        value
+    log
+
+let logMsg = makeLog printf
+let logMsgn = makeLog printfn
+
+let a = (logMsgn "ciao" 12)
+printfn "a=%A" a
+
+
+let mult3ThenSquareLogged =
+    logMsg "Input value = "
+    >> multBy3
+    >> logMsg "Multiplied by 3 ="
+    >> square
+    >> logMsgn "After the addiction ="
+
+printfn "2 * 3 squared = %d" (mult3ThenSquareLogged 2)
