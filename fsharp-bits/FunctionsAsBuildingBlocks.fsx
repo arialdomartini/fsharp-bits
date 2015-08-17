22f3ef6d98c544b41041d2a9a99b80e1bf22aae0
Arialdo Martini <arialdomartini@gmail.com>
2015-08-17 17:16:15 +0200

    Composing 2 functions

--- fsharp-bits/FunctionsAsBuildingBlocks.fsx
+++ fsharp-bits/FunctionsAsBuildingBlocks.fsx
@@ -1,7 +1,11 @@
 ﻿let add2 x = x + 2
-let multiplicateBy2 x = x * 2
+let multBy3 x = x * 3
 let square x = x * x
 
 [1..10] |> List.map  add2 |> printfn "%A"
-[1..10] |> List.map  multiplicateBy2 |> printfn "%A"
+[1..10] |> List.map  multBy3 |> printfn "%A"
 [1..10] |> List.map  square |> printfn "%A"
+
+let add2ThenMultBy3 x = x |> add2 |> multBy3
+
+[1..10] |> List.map add2ThenMultBy3 |> printfn "%A"
