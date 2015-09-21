82deeb5c70781ddad2a9b73a703b632e96c83368
Arialdo Martini <arialdomartini@gmail.com>
2015-09-21 06:52:15 +0200

    A generic function implementing the Decorator pattern

--- fsharp-bits/Decorator.fsx
+++ fsharp-bits/Decorator.fsx
@@ -12,3 +12,17 @@ printfn "Result: %d" (addingCalculator 10)
 printfn "Using loggingCalculator"
 printfn "Result:"
 loggingCalculator addingCalculator 10
+
+let genericLoggerWrapper (funct:int->int) : int->int =
+    let loggingDecoratedFunction(input:int) : int =
+        printfn "input is %d" input
+        let result = funct input
+        printfn "result is %d" result
+        result
+    loggingDecoratedFunction
+
+let loggingCalculatorGenerated = (genericLoggerWrapper addingCalculator)
+
+printfn "Using loggingCalculatorGenerated"
+printfn "%d" (loggingCalculatorGenerated 10)
+
