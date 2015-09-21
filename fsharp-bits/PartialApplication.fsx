07bb4689a41dd1e56c0a6244191409d8f8f8ad74
Arialdo Martini <arialdomartini@gmail.com>
2015-09-21 19:05:35 +0200

    Partial application

---
+++ fsharp-bits/PartialApplication.fsx
@@ -0,0 +1,15 @@
+﻿let add x y = x + y
+let result = add 1 2
+
+let add2 = add 2
+
+printfn "add2 10 should be 12: %d" (add2 10)
+
+let makeAdder n = 
+    let addn y = 
+        n + y
+    addn
+
+let add2_2 = makeAdder 2
+printfn "add2_2 10 should be 12: %d" (add2_2 10)
+
