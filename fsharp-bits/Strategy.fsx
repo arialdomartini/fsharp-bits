efdbcdb5a56c5f0e763930c7cba583227a49d48b
Arialdo Martini <arialdomartini@gmail.com>
2015-09-21 06:58:20 +0200

    Strategy pattern

---
+++ fsharp-bits/Strategy.fsx
@@ -0,0 +1,7 @@
+﻿type Animal(noiseMakingStrategy) =
+    member this.MakeNoise =
+        noiseMakingStrategy() |> printfn "Making the noise %s"
+
+let meowing() = "Meow"
+let cat = Animal(meowing)
+cat.MakeNoise
