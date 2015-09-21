ed65e4d1a887fdcba4fd2fbeb8abed81a10526eb
Arialdo Martini <arialdomartini@gmail.com>
2015-09-21 06:38:06 +0200

    Decorator pattern the FP way

---
+++ fsharp-bits/Decorator.fsx
@@ -0,0 +1,14 @@
+﻿let addingCalculator input = input + 1
+
+let loggingCalculator innerCalculator input = 
+    printfn "logging calculator received input %d" input
+    let result = innerCalculator input
+    printfn "And result is %d" result
+    result
+
+printfn "Using addingCalculator"
+printfn "Result: %d" (addingCalculator 10)
+
+printfn "Using loggingCalculator"
+printfn "Result:"
+loggingCalculator addingCalculator 10
