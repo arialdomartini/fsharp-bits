abee00a7b0ebdc9bf8609060a0b05c1d4801e62a
Arialdo Martini <arialdomartini@gmail.com>
2015-09-22 07:39:59 +0200

    Using Partial application to implement Decorator Pattern

--- fsharp-bits/PartialApplication.fsx
+++ fsharp-bits/PartialApplication.fsx
@@ -13,3 +13,17 @@ let makeAdder n =
 let add2_2 = makeAdder 2
 printfn "add2_2 10 should be 12: %d" (add2_2 10)
 
+let genericLoggerDecorator before after func input = 
+    before input
+    let result = func input
+    after result
+    result
+
+let notDecorated value = 
+    printfn "I got the value %A" value
+    value + " yeah!"
+let logging value = 
+    printfn "logging %A" value
+let decorated = genericLoggerDecorator logging logging notDecorated
+
+decorated "foo"
