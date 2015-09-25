2ac04623278c4f962692c30e3f823610302d984c
Arialdo Martini <arialdomartini@gmail.com>
2015-09-25 06:32:00 +0200

    exploring immutability

---
+++ fsharp-bits/Immutability.fsx
@@ -0,0 +1,16 @@
+﻿type PersonName = {FirstName:string; SecondName:string}
+let john = {FirstName = "John"; SecondName = "Miner"}
+
+// Copying objects
+let mary = {john with FirstName = "Mary"}
+let john_copy = {john with FirstName = "John"}
+
+assert System.Object.ReferenceEquals(john, john_copy)
+
+
+let list = [1;2;3;4]
+let prepended_list = 0 :: list
+let like_list = list.Tail
+
+assert System.Object.ReferenceEquals(list, like_list)
+
