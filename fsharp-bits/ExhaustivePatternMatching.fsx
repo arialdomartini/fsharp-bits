0aa9bea89a810fbdd2d764a092cf2a7fcc9d2401
Arialdo Martini <arialdomartini@gmail.com>
2015-09-25 06:44:55 +0200

    Playing with exhaustive pattern matching and options

---
+++ fsharp-bits/ExhaustivePatternMatching.fsx
@@ -0,0 +1,15 @@
+﻿let getFileInfo filePath = 
+    let fi = System.IO.FileInfo filePath
+    if fi.Exists then Some(fi) else None
+
+
+let goodFilename = "existingFile.txt"
+let badFilename = "notExistingFile.txt"
+
+
+let doSomethingWithOption(fileInfo:System.IO.FileInfo option) =
+    match fileInfo with
+    | Some(info) -> printfn "%s" info.DirectoryName
+    | None       -> printfn "Missing fileinfo"
+
+doSomethingWithOption(getFileInfo "somefile")
