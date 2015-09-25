baeec9d16f8a263a1cb9b65ee73ecbee2ec5ec74
Arialdo Martini <arialdomartini@gmail.com>
2015-09-25 19:16:49 +0200

    A more complete example with Exhaustive Pattern Matching

--- fsharp-bits/ExhaustivePatternMatching.fsx
+++ fsharp-bits/ExhaustivePatternMatching.fsx
@@ -1,4 +1,5 @@
-﻿let getFileInfo filePath = 
+﻿
+let getFileInfo filePath = 
     let fi = System.IO.FileInfo filePath
     if fi.Exists then Some(fi) else None
 
@@ -20,4 +21,46 @@ let rec movingAverages list =
     | [] -> []
     | x :: y :: rest -> [ (x+y)/2.0] :: movingAverages(y::rest)
     | [_] -> []  // this can't be skipped: pattern matching is required to be exhaustive
-    
+
+
+ 
+
+type Result<'a, 'b> =
+    | Success of 'a
+    | Failure of 'b
+
+type FileErrorReason =
+    | FileNotFound of string
+    | UnauthorizedAccess of string * System.Exception
+
+
+let performActionOnFile action filePath =
+    try
+        use streamReader = new System.IO.StreamReader(filePath:string)
+        let result = action streamReader
+        streamReader.Close()
+
+        Success(result)
+    with
+        | :? System.IO.FileNotFoundException as ex   -> Failure(FileNotFound filePath)
+        | :? System.Security.SecurityException as ex -> Failure(UnauthorizedAccess(filePath, ex))
+
+
+let middleLayerDo action filePath =
+      let fileResult = performActionOnFile action filePath
+      fileResult
+
+
+let topLayerDo action filePath =
+    let fileResult = middleLayerDo action filePath
+    fileResult
+
+let entryPoint =
+    let result = topLayerDo (fun fr -> fr.ReadToEnd) "foo.txt"
+    match result with
+    | Success filePath -> "%A Success"
+    | Failure reason  -> 
+        match reason with
+        | FileNotFound file -> "File not found"
+        | UnauthorizedAccess (file, ex) -> "Unauthorized exception"
+
