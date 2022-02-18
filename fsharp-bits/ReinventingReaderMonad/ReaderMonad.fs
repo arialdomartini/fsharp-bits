19a09940bc4f11a5688cc3fa22782b30863cade8
Arialdo Martini <arialdomartini@kpmg.com>
2022-02-18 12:16:08 +0100

    Result map and bind in Result module

--- fsharp-bits/ReinventingReaderMonad/ReaderMonad.fs
+++ fsharp-bits/ReinventingReaderMonad/ReaderMonad.fs
@@ -10,19 +10,6 @@ type CustomerId = CustomerId of string
 type ProductId = ProductId of string
 type ProductInfo = { Name : string }
 
-
-let map (f : 'a -> 'b) (r : Result<'a>) : Result<'b> =
-    match r with
-    | Failure f -> Failure f
-    | Success v -> Success (f v)
-
-let bind (f : 'a -> Result<'b>) (r : Result<'a>) : Result<'b> =
-    match r with
-    | Failure f -> Failure f
-    | Success v -> f v
-
-
-
 [<Fact>]
 let ``api client can save and retrieve values``() =
     let api = ApiClient()
--- fsharp-bits/ReinventingReaderMonad/Result.fs
+++ fsharp-bits/ReinventingReaderMonad/Result.fs
@@ -1,3 +1,14 @@
 ﻿module FSharpBits.ReinventingReaderMonad.Result
 
 type Result<'a> = Success of 'a | Failure of string list
+
+
+let map (f : 'a -> 'b) (r : Result<'a>) : Result<'b> =
+    match r with
+    | Failure f -> Failure f
+    | Success v -> Success (f v)
+
+let bind (f : 'a -> Result<'b>) (r : Result<'a>) : Result<'b> =
+    match r with
+    | Failure f -> Failure f
+    | Success v -> f v
