8c098a599d084b28f6788b3f6031633d6674195a
Arialdo Martini <arialdomartini@kpmg.com>
2023-08-09 19:17:21 +0200

    Running FsCheck from Expecto

---
+++ fsharp-bits/PropertyBasedTesting/BasicFsCheckTest.fs
@@ -0,0 +1,16 @@
+﻿module FSharpBits.PropertyBasedTesting.BasicFsCheckTest
+
+open FsCheck
+open Expecto
+
+let gen = Gen.oneof [ gen { return true }; gen { return false } ] |> Arb.fromGen
+
+[<Tests>]
+let treeTests =
+    testList
+        "True | False"
+        [ test "fail please" { Tests.failtest "OMG I failed!" }
+          testProperty "true|false is always true" (Prop.forAll gen (fun b -> b = true))
+              
+          test "is really true|false is always true" {
+              Check.QuickThrowOnFailure (Prop.forAll gen (fun b -> b = true)) } ]
--- fsharp-bits/PropertyBasedTesting/BasicHedgehogTest.fs
+++ fsharp-bits/PropertyBasedTesting/BasicHedgehogTest.fs
@@ -8,7 +8,6 @@ let treeTests =
     testList
         "Tree tests"
         [
-
           testCase "classic rev.rev.list"
           <| fun _ ->
               property {
