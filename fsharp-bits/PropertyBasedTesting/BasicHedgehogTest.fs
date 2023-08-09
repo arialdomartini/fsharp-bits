20c7d422102683e6fc6ac549a3f1d5c20d2e8e02
Arialdo Martini <arialdomartini@kpmg.com>
2023-08-09 21:43:57 +0200

    Tests with Hedgehog using generators, with and without computation expressions

--- fsharp-bits/PropertyBasedTesting/BasicHedgehogTest.fs
+++ fsharp-bits/PropertyBasedTesting/BasicHedgehogTest.fs
@@ -4,16 +4,25 @@ open Hedgehog
 open Expecto
 
 let someProperty xs = xs |> List.rev |> List.rev = xs
+let someProp x = x + 1 = x
+
+let gen = Gen.int32 (Range.linear 0 100)
 
 [<Expecto.Tests>]
 let treeTests =
     testList
         "Tree tests"
         [
-          testCase "classic rev.rev.list"
+          testCase "x != x+1"
           <| fun _ ->
               property {
-                  let! xs = Gen.list (Range.linear 0 100) Gen.alpha
-                  return someProperty xs
+                  let! xs = gen
+                  return someProp xs
               }
-              |> Property.checkBool ]
+              |> Property.checkBool
+              
+          testCase "x != x+1 without computation expression"
+          <| fun _ ->
+              let prop = Property.forAll (fun x -> property.Return (someProp x)) gen  
+              Property.check prop
+         ]
