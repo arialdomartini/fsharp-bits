e73690b4040705cb72dcb1825b5ab132c63b55b6
Arialdo Martini <arialdomartini@kpmg.com>
2023-08-13 00:31:53 +0200

    Birthday greetings kata: sends 1 hardcoded email per friend

---
+++ fsharp-bits/PropertyBasedTesting/WithFsCheck/BirthdayGreetings.fs
@@ -0,0 +1,22 @@
+﻿module FSharpBits.PropertyBasedTesting.WithFsCheck.BirthdayGreetings
+
+open Expecto
+open FsCheck
+
+let toEmail _ = "Happy birthday!"
+
+let greeter friends  = friends |> Seq.map toEmail
+
+
+[<Tests>]
+let fizzBuzzTests =
+    testProperty
+        "sends the same email to each friend"
+        (let friends = Arb.generate<string> |> Gen.listOf |> Arb.fromGen
+         
+         let props f =
+             let emails = greeter f
+             Seq.length emails = Seq.length f &&
+             emails |> Seq.forall (fun e -> e = "Happy birthday!")
+
+         Prop.forAll friends props)
