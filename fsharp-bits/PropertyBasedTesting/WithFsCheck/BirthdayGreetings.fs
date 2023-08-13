bfe35941477743b228e3ffd68d95f9cb3fc3733e
Arialdo Martini <arialdomartini@kpmg.com>
2023-08-13 14:24:26 +0200

    Only sends emails to friends born today

--- fsharp-bits/PropertyBasedTesting/WithFsCheck/BirthdayGreetings.fs
+++ fsharp-bits/PropertyBasedTesting/WithFsCheck/BirthdayGreetings.fs
@@ -1,20 +1,74 @@
 ﻿module FSharpBits.PropertyBasedTesting.WithFsCheck.BirthdayGreetings
 
+open System
 open Expecto
 open FsCheck
 
+type Friend = { BirthDate: DateTime }
+
 let toEmail _ = "Happy birthday!"
 
-let greet friends = friends |> Seq.map toEmail
+let toDays years = years * 365
+let maxAge = 120 |> toDays
 
-let friends: Arbitrary<string list> = Arb.generate<string list> |> Arb.fromGen
+let born date (friend: Friend) = friend.BirthDate = date 
 
-let theSameEmailWasSent friends: bool =
-   let emails = greet friends
-   Seq.length emails = Seq.length friends
-   && emails |> Seq.forall (fun e -> e = "Happy birthday!")
+let greetFriends (friends: Friend list) today =
+    friends
+    |> Seq.filter (born today)
+    |> Seq.map toEmail
+
+let friends: Arbitrary<string list> = Arb.from<string list>
+
+let randomDate = Arb.toGen Arb.from<DateTime>
+let randomAgeInDays: Gen<int> = Gen.choose (1, maxAge)
+
+let randomFriendBornInDate (birthday: DateTime) : Gen<Friend> =
+    gen {
+        let! date = Gen.constant birthday
+        return { Friend.BirthDate = date }
+    }
+
+let friendsBornToday (today: DateTime) : Gen<Friend list> =
+    (randomFriendBornInDate today) |> Gen.listOf
+
+let friendsNotBornToday: Gen<Friend list> =
+    gen {
+        let! randomDate = randomDate
+        let friend = randomFriendBornInDate randomDate
+        let! friends = Gen.listOf friend
+        return friends
+    }
+
+type RandomGen =
+    { Today: DateTime
+      BornToday: Friend list
+      AllFriends: Friend list }
+
+let myGroupOfFriends: Arbitrary<RandomGen> =
+    gen {
+        let! today = randomDate
+        let! friendsBornToday = friendsBornToday today
+        let! friendsNotBornToday = friendsNotBornToday
+        let allOfThem = List.concat [ friendsBornToday; friendsNotBornToday ]
+
+        return
+            { Today = today
+              BornToday = friendsBornToday
+              AllFriends = allOfThem }
+    }
+    |> Arb.fromGen
+
+let theSameEmailWasSentToTheOnesBornToday (randomGen: RandomGen) =
+    let today = randomGen.Today
+    let emails = greetFriends randomGen.AllFriends today
+
+    Seq.length emails = List.length (randomGen.BornToday)
+    && emails |> Seq.forall (fun e -> e = "Happy birthday!")
+
 
 [<Tests>]
 let birthdayGreetingsKataTests =
-    testList "Birthday Greetings Kata tests"
-        [ testProperty "sends the same email to each friend" (Prop.forAll friends theSameEmailWasSent) ]
+    testList
+        "Birthday Greetings Kata tests"
+        [ testProperty "sends the same email to all friends having birthday today" (Prop.forAll myGroupOfFriends theSameEmailWasSentToTheOnesBornToday) ]
