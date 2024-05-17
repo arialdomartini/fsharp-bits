3d4241e07739b7a0ae73a71b6c24e70c649de808
Arialdo Martini <arialdomartini@kpmg.com>
2024-05-17 10:45:32 +0200

    Indexing a tree, with WithCounter

--- fsharp-bits/StateMonad/StateMonadTest.fs
+++ fsharp-bits/StateMonad/StateMonadTest.fs
@@ -1,6 +1,7 @@
 ﻿module FSharpBits.StateMonad.StateMonadTest
 
 open Xunit
+open Swensen.Unquote
 
 type GTree<'c> =
     | GLeaf of 'c
@@ -10,10 +11,19 @@ type Tree =
     | Leaf of string
     | Node of (Tree * Tree)
 
+type ITree =
+    | ILeaf of (string * int)
+    | INode of (ITree * ITree)
+
 let rec countLeaves tree =
     match tree with
     | Leaf l -> 1
     | Node(l, r) -> countLeaves l + countLeaves r
+
+
+type MyGeneric<'a> = MyGeneric of 'a
+
+let x: MyGeneric<int> = MyGeneric 12
 
 [<Fact>]
 let ``counting tree leaves`` () =
@@ -22,20 +32,57 @@ let ``counting tree leaves`` () =
 
     let numberOfLeaves = countLeaves tree
 
-    Assert.Equal(3, numberOfLeaves)
+    test <@ numberOfLeaves = 3 @>
 
 let rec mapTree (f: 'a -> 'b) : (GTree<'a> -> GTree<'b>) =
     fun tree ->
         match tree with
-        | GLeaf v -> GLeaf (f v)
-        | GNode (l, r) -> GNode ((mapTree f l), (mapTree f r)) 
-        
-        
+        | GLeaf v -> GLeaf(f v)
+        | GNode(l, r) -> GNode((mapTree f l), (mapTree f r))
+
+
 [<Fact>]
 let ``mapping a tree`` () =
-    let tree               = GNode(GLeaf "one", GNode(GLeaf "two", GLeaf "three"))
-    
+    let tree = GNode(GLeaf "one", GNode(GLeaf "two", GLeaf "three"))
+
     let mappedToIntegers = mapTree String.length tree
-    let containingIntegers = GNode(GLeaf 3, GNode(GLeaf 3, GLeaf 5))
- 
-    Assert.Equal(containingIntegers, mappedToIntegers)
+    let containingLengthOfWords = GNode(GLeaf 3, GNode(GLeaf 3, GLeaf 5))
+
+    test <@ mappedToIntegers = containingLengthOfWords @>
+
+
+type WithCounter =
+    WithCounter of (int -> ITree * int)
+
+let (>>!) (WithCounter f) counter = f counter
+
+
+
+let (>>>) (f : WithCounter) (g : ITree -> WithCounter) : WithCounter =
+    WithCounter (
+        fun counter->
+            let rf, cf = f >>! counter
+            let rg, cg = g rf >>! cf
+            (rg, cg))
+
+
+let pure' v =
+    WithCounter (fun counter -> (v, counter))
+
+    
+let rec indexTree tree : WithCounter =
+    match tree with
+    | Leaf v -> WithCounter (fun counter -> (ILeaf(v, counter), counter + 1))
+    | Node(l, r) ->
+        (indexTree l)
+        >>> (fun rl ->
+            let rr = indexTree r
+            rr
+            >>> fun rrr -> (pure' (INode (rl, rrr))))
+        
+[<Fact>]
+let ``indexes a tree`` () =
+    let tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+    let withIndexes = INode(ILeaf("one", 1), INode(ILeaf("two", 2), ILeaf("three", 3)))
+
+    test <@ fst (indexTree tree >>! 1) = withIndexes @>
