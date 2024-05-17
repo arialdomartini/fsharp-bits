990799d2d2b699485663fa9045a6d84186682331
Arialdo Martini <arialdomartini@kpmg.com>
2024-05-17 07:22:54 +0200

    StateMonad: counting leaves

---
+++ fsharp-bits/StateMonad/StateMonadTest.fs
@@ -0,0 +1,21 @@
+﻿module FSharpBits.StateMonad.StateMonadTest
+
+open Xunit
+
+type Tree =
+    | Leaf of string
+    | Node of (Tree * Tree)
+
+let rec countLeaves tree =
+    match tree with
+    | Leaf l -> 1
+    | Node(l, r) -> countLeaves l + countLeaves r
+
+[<Fact>]
+let ``counting tree leaves`` () =
+
+    let tree: Tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+
+    let numberOfLeaves = countLeaves tree
+
+    Assert.Equal(3, numberOfLeaves)
