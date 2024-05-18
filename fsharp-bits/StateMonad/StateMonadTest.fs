e24e34fe9018ec5f6849313e058238df7cf64d88
Arialdo Martini <arialdo.martini@gmail.com>
2024-05-18 18:33:14 +0200

    Indexing a tree, manually

--- fsharp-bits/StateMonad/StateMonadTest.fs
+++ fsharp-bits/StateMonad/StateMonadTest.fs
@@ -44,3 +44,19 @@ let ``changing tree leaves content`` () =
     let treeOfLengths = Node(Leaf 3, Node(Leaf 3, Leaf 5))
 
     test <@ containingLengths = treeOfLengths @>
+
+let rec index tree count =
+    match tree with
+    | Leaf v -> (Leaf (v, count), count + 1)
+    | Node (l, r) ->
+        let (vl, cl) = index l count
+        let (vr, cr) = index r cl
+        (Node (vl, vr), cr)
+
+[<Fact>]
+let ``indexes a tree``() =
+    let tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+    let indexed = index tree 1
+    let withIndexes = Node(Leaf ("one", 1), Node(Leaf ("two", 2), Leaf ("three", 3)))
+    
+    test <@ fst indexed = withIndexes @>
