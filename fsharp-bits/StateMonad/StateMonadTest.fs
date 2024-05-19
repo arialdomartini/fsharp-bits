75d65288fe0bf054380fdc8260b0f8569e9564ce
Arialdo Martini <arialdo.martini@gmail.com>
2024-05-19 10:40:27 +0200

    Node with Applicative

--- fsharp-bits/StateMonad/StateMonadTest.fs
+++ fsharp-bits/StateMonad/StateMonadTest.fs
@@ -6,6 +6,8 @@ open Swensen.Unquote
 type Tree<'v> = Leaf of 'v | Node of Node<'v>
 and Node<'v> = Tree<'v> * Tree<'v>
 
+let buildNode left right = Node (left, right)
+
 // Tree 'v -> int
 let rec countLeaves tree =
     match tree with
@@ -23,18 +25,46 @@ let (^) = map
 
 [<Fact>]
 let ``count tree leaves`` () =
-    let tree = Node(Node(Leaf("One"), Leaf("Two")), Leaf "Three")
+    let tree = Node(Node(Leaf "One", Leaf "Two"), Leaf "Three")
     
     let numberOfLeaves = countLeaves tree
     
     test <@ numberOfLeaves = 3 @>
 
-
 [<Fact>]
 let ``count length of leaves value`` () =
-    let tree = Node(Node(Leaf("One"), Leaf("Two")), Leaf "Three")
-    let treeOfNumbers = Node(Node(Leaf(3), Leaf(3)), Leaf 5)
+    let tree = Node(Node(Leaf "One", Leaf "Two"), Leaf "Three")
+    let treeOfNumbers = Node(Node(Leaf 3, Leaf 3), Leaf 5)
     
     let treeOfLengths = String.length^ tree
     
     test <@ treeOfLengths = treeOfNumbers @>
+
+type WithCounter<'v> = WithCounter of (int -> 'v * int)
+
+let run (WithCounter f) counter = f counter
+let pure' v = WithCounter (fun counter -> (v, counter))
+
+type ITree = Tree<string * int>
+
+let (<*>) (f : WithCounter<'a -> 'b>) (a: WithCounter<'a>) : WithCounter<'b> =
+    WithCounter (fun counter ->
+        let rf, cf = run f counter
+        let ra, ca = run a cf
+        let ra = rf ra
+        (ra, ca))
+
+let rec index tree =
+    match tree with
+    | Leaf v -> WithCounter (fun count -> Leaf (v, count), count+1)
+    | Node (l, r) ->
+        pure' buildNode <*> index l <*> index r
+
+[<Fact>]
+let ``index tree`` () =
+    let tree = Node(Node(Leaf "One", Leaf "Two"), Leaf "Three")
+    let treeWithIndexes = Node(Node(Leaf("One", 1), Leaf("Two", 2)), Leaf ("Three", 3))
+    
+    let indexedTree = run (index tree) 1
+    
+    test <@ fst indexedTree = treeWithIndexes @>
