31f58b9ead3a1cac720e21f702d45008025090d9
Arialdo Martini <arialdomartini@kpmg.com>
2024-05-30 23:47:40 +0200

    bind for parser

--- fsharp-bits/StateMonad/ParserCombinator.fs
+++ fsharp-bits/StateMonad/ParserCombinator.fs
@@ -4,16 +4,6 @@ open FSharpBits.StateMonad.StateMonadForParserCombinator
 open Xunit
 open Swensen.Unquote
 
-module Li =
-    let (<**>) (fs : ('a -> 'b) list) (vs: 'a list) : 'b list =
-        seq {
-            for f in fs do
-            for v in vs do
-            yield f v
-        } |> Seq.toList
-    let ap = (<**>)
-
-open Li
 
 let state = StateComputation()
 
@@ -26,13 +16,34 @@ let runParser p v =
 let mapParser f p =
     let flist = List.map f
     map flist p
+
+let (<**>) (fs : ('a -> 'b) list) (vs: 'a list) : 'b list =
+    seq {
+        for f in fs do
+        for v in vs do
+        yield f v
+    } |> Seq.toList
     
 let (<**>) (f: Parser<'a -> 'b>) (m: Parser<'a>) : Parser<'b> =
     State (fun (s : string) ->
         let sf, vf = runParser f s
         let sm, vm = runParser m sf
-        let vvf = ap vf vm
+        let vvf = vf <**> vm
         (sm, vvf))
+    
+let folder acc st =
+    let stateToApply = fst acc
+    let elementsSoFar = snd acc
+    let ns, nr = runParser st stateToApply
+    (ns, List.append elementsSoFar nr)
+    
+let (>>>=) (m: Parser<'a>) (f: 'a -> Parser<'b>) : Parser<'b> =
+    State (fun s ->
+        let sm, vm = runParser m s
+        let parsers: Parser<'b> list = List.map f vm
+        let initialState = (sm, [])
+        let s, objects = List.fold folder initialState parsers
+        s, objects)
     
 [<Fact>]
 let ``run parser`` () =
@@ -56,3 +67,13 @@ let ``applicative parser`` () =
     let mapped = f <**> parser
     let s, v = runParser mapped "original-"
     (s, v) =! ("original-func-parsed", [2; 4; 6; 3; 6; 9])
+
+// Parser a -> (a -> Parser b) -> Parser b
+// m >>= f
+[<Fact>]
+let ``parser bind`` () =
+    let parser: Parser<string> = State (fun s -> (s + "parsed-", ["one";"two";"three"]))
+    let f n = State (fun s -> (s + "bound-" + n, [String.length s]))
+    let bound = parser >>>= f
+    let s, v = runParser bound "original-"
+    (s, v) =! ("original-parsed-bound-onebound-twobound-three", [16; 25; 34])) 
