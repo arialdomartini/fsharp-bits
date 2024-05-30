da03d93a85ed337ca3c9e6000a76300f58ccc745
Arialdo Martini <arialdomartini@kpmg.com>
2024-05-30 07:07:54 +0200

    runState, execState

---
+++ fsharp-bits/StateMonad/ParserCombinator.fs
@@ -0,0 +1,23 @@
+﻿module FSharpBits.StateMonad.ParserCombinator
+
+open Swensen.Unquote
+open Xunit
+
+type State<'s, 'v> = State of ('s -> 's * 'v)
+
+let runState (State f) s = f s
+let execState m = snd << (runState m)
+
+//type Parser<'a> = State<string list, 'a>
+
+let sm = State (fun s -> (s+1, s * 10))
+
+[<Fact>]
+let ``runState State`` () =
+  let v, ns = runState sm 42
+  (v, ns) =! (43, 420)
+
+[<Fact>]
+let ``execState State`` () =
+  let ns = execState sm 42
+  ns =! 420
