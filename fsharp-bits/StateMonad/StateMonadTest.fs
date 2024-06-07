625da41dbbdd835900c25a3ff3154dc3b35e9e50
Arialdo Martini <arialdomartini@kpmg.com>
2024-06-07 14:40:35 +0200

    Cacciucco e Deploy: punto di partenza

--- fsharp-bits/StateMonad/StateMonadTest.fs
+++ fsharp-bits/StateMonad/StateMonadTest.fs
@@ -3,78 +3,16 @@
 open Xunit
 open Swensen.Unquote
 
-type Tree<'a> =
-    | Leaf of 'a
-    | Node of (Tree<'a> * Tree<'a>)
 
-type State<'a> = State of (int -> 'a * int)
+//     Leaf
 
-let run (State f) s = f s
+//       Node
+//       /  \
+//     Leaf Leaf
 
-let (<*>) f v =
-    State(fun s ->
-        let fv, fs = run f s
-        let vv, vs = run v fs
-        (fv vv, vs))
 
-let rec map f sm =
-    State(fun s ->
-        let v, s' = run sm s
-        f v, s')
-
-let build l r = Node(l, r)
-let (^) = map
-
-let (<*) f v =
-    State(fun s ->
-        let fv, fs = run f s
-        let _, s' = run v fs
-        (fv, s'))
-
-let get = State(fun s -> (s, s))
-let put v = State(fun _ -> ((), v))
-let inc = State(fun s -> ((), s + 1))
-
-let get = State(fun s -> (s, s))
-let put v = State(fun _ -> ((), v))
-let inc = State(fun s -> ((), s + 1))
-let pure' v = State(fun s -> (v, s))
-let buildLeaf v s = Leaf (v, s)
-
-let (>>=) v f =
-    State (fun s ->
-        let vv, sv = run v s
-        let fv, fs = run (f vv) sv
-        fv, fs)
-
-type KeepTrack() =
-    member this.Bind(v, f) = v >>= f
-    member this.Return v = pure' v
-let keepTrack = KeepTrack()
-
-let rec index =
-    function
-    // | Leaf a -> State (fun s -> (Leaf (a, s), s + 1))
-    | Leaf a -> buildLeaf^ (pure' a) <*> get <* inc
-    // | Node(l, r) ->
-    //     let li = index l
-    //     let ri = index r
-    //     build ^ li <*> ri
-    // | Node(l, r) ->
-    //     index l
-    //       >>= (fun lv -> (index r >>= (fun rv -> pure' (build lv rv))))
-    | Node(l, r) ->
-        keepTrack{
-            let! li = index l
-            let! ri = index r
-            return (build li ri)
-        }
-        
-[<Fact>]
-let ``indexes a tree`` () =
-    let tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
-    let withIndexes = Node(Leaf("one", 1), Node(Leaf("two", 2), Leaf("three", 3)))
-
-    let indexed, _ = run (index tree) 1
-
-    test <@ indexed = withIndexes @>
+//          Node
+//         /    \
+//      Leaf     Node
+//              /   \
+//            Leaf  Leaf
---
+++ fsharp-bits/StateMonad/StateMonadWithComputationExpressionTest.fs
@@ -0,0 +1,106 @@
+﻿module FSharpBits.StateMonad.StateMonadWithParserCombinatorTest
+
+open Xunit
+open Swensen.Unquote
+
+type GTree<'c> =
+    | GLeaf of 'c
+    | GNode of GNode<'c>
+
+and GNode<'c> = GTree<'c> * GTree<'c>
+
+type Tree =
+    | Leaf of string
+    | Node of Node
+
+and Node = Tree * Tree
+
+type ITree<'v> =
+    | ILeaf of ('v * int)
+    | INode of INode<'v>
+
+and INode<'v> = ITree<'v> * ITree<'v>
+
+let rec countLeaves tree =
+    match tree with
+    | Leaf l -> 1
+    | Node(l, r) -> countLeaves l + countLeaves r
+
+
+type MyGeneric<'a> = MyGeneric of 'a
+
+let x: MyGeneric<int> = MyGeneric 12
+
+[<Fact>]
+let ``counting tree leaves`` () =
+
+    let tree: Tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+
+    let numberOfLeaves = countLeaves tree
+
+    test <@ numberOfLeaves = 3 @>
+
+let rec mapTree (f: 'a -> 'b) : (GTree<'a> -> GTree<'b>) =
+    fun tree ->
+        match tree with
+        | GLeaf v -> GLeaf(f v)
+        | GNode(l, r) -> GNode((mapTree f l), (mapTree f r))
+
+
+[<Fact>]
+let ``mapping a tree`` () =
+    let tree = GNode(GLeaf "one", GNode(GLeaf "two", GLeaf "three"))
+
+    let mappedToIntegers = mapTree String.length tree
+    let containingLengthOfWords = GNode(GLeaf 3, GNode(GLeaf 3, GLeaf 5))
+
+    test <@ mappedToIntegers = containingLengthOfWords @>
+
+type WithCounter<'v> = WithCounter of (int -> 'v * int)
+
+let (>>!) (WithCounter f) (counter: int) = f counter
+
+let (>>=) (f: WithCounter<'v>) (g: 'v -> WithCounter<'w>) : WithCounter<'w> =
+    WithCounter(fun counter ->
+        let rf, cf = f >>! counter
+        let rg, cg = g rf >>! cf
+        (rg, cg))
+
+let pure' v =
+    WithCounter(fun counter -> (v, counter))
+
+type IndexTreeComputation() =
+    member this.Bind(m, f) = (m >>= f)
+
+    member this.Return(v) = pure' v
+
+let keepTrack = IndexTreeComputation()
+//let keepTrack = StatefulBuilder()
+
+let mapState (from: WithCounter<'v>) (f: int -> int) : WithCounter<'v> =
+    WithCounter(fun counter -> from >>! (f counter))
+
+let getCounter = WithCounter(fun c -> (c, c))
+let incCounter = WithCounter(fun c -> ((), c + 1))
+let setCounter v = WithCounter(fun _ -> ((), v))
+
+let rec indexTree = function
+    | Leaf v ->
+        keepTrack {
+            let! counter = getCounter
+            do! setCounter (counter + 1)
+            return ILeaf(v, counter)
+        }
+    | Node(l, r) ->
+        keepTrack {
+            let! leftIndexed = indexTree l
+            let! rightIndexed = indexTree r
+            return INode(leftIndexed, rightIndexed)
+        }
+
+[<Fact>]
+let ``indexes a tree`` () =
+    let tree = Node(Leaf "one", Node(Leaf "two", Leaf "three"))
+    let withIndexes = INode(ILeaf("one", 1), INode(ILeaf("two", 2), ILeaf("three", 3)))
+
+    test <@ fst (indexTree tree >>! 1) = withIndexes @>
