3a2ff756f002368361238134abc566f9d22334b1
Arialdo Martini <arialdo.martini@gmail.com>
2025-04-16 08:26:39 +0200

    try combinator

---
+++ fsharp-bits/ParserCombinators/Try.fs
@@ -0,0 +1,38 @@
+module FSharpBits.ParserCombinators.Try
+
+open FSharpBits.ParserCombinators.ParserCombinators
+open Xunit
+open Swensen.Unquote
+
+let ``try`` (parser: Parser<'a>) : Parser<'a> =
+    Parser (fun input ->
+        let _, result as r = runParser parser input
+        match result with
+        | Ok _ -> r
+        | Error _ -> (input, Error { Expected = "to apply a parser"; Encountered = "something different. Backtracking." }))
+
+
+[<Fact>]
+let ``return result if successful`` () =
+    let successful = Parser (fun _ -> ("", Ok "success"))
+
+    let trying = ``try`` successful
+
+    let _, result = runParser trying "input"
+
+    test <@ result = Ok "success" @>
+
+let someError = Error { Expected = ""; Encountered = "" }
+
+[<Fact>]
+let ``does not consume input in case it fails`` () =
+    let failing = Parser (fun _ ->  ("", someError))
+
+    let trying = ``try`` failing
+
+    let originalInput = "input"
+    let rest, result = runParser trying originalInput
+
+    let isError = result.IsError
+    test <@ isError @>
+    test <@ rest = originalInput @>
