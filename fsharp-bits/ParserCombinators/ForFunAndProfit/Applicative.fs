00288e9727ccadfec55849fdb1c7ce5a36acc579
Arialdo Martini <arialdo.martini@gmail.com>
2025-04-17 21:10:51 +0200

    lift2 and lift3

--- fsharp-bits/ParserCombinators/ForFunAndProfit/Applicative.fs
+++ fsharp-bits/ParserCombinators/ForFunAndProfit/Applicative.fs
@@ -30,5 +30,18 @@ let parse3Digits: Parser<int> =
 
 [<Fact>]
 let ``applicative functor`` () =
-
     test <@ run parse3Digits "123ZZZ" = Success (123, "ZZZ")@>
+
+
+let lift2 (f: 'f -> 'a -> 'b) : 'f Parser -> 'a Parser -> 'b Parser =
+    fun a b -> f <!> a <*> b
+
+let lift3 (f: 'f -> 'a -> 'b -> 'c) : 'f Parser -> 'a Parser -> 'b Parser -> 'c Parser=
+    fun a b c -> f <!> a <*> b <*> c
+
+let parse3Digits' =
+    lift3 combines3Digits parseDigit parseDigit parseDigit
+
+[<Fact>]
+let ``with lift3`` () =
+    test <@ run parse3Digits' "123ZZZ" = Success (123, "ZZZ")@>
