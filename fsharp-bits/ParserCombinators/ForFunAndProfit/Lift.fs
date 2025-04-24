79b9449a95a1c1e3409d6e749549c49edca53481
Arialdo Martini <arialdo.martini@gmail.com>
2025-04-24 08:17:55 +0200

    sequence

--- fsharp-bits/ParserCombinators/ForFunAndProfit/Lift.fs
+++ fsharp-bits/ParserCombinators/ForFunAndProfit/Lift.fs
@@ -2,21 +2,51 @@ module FSharpBits.ParserCombinators.ForFunAndProfit.Lift
 
 open FSharpBits.ParserCombinators.ForFunAndProfit.Functor
 open FSharpBits.ParserCombinators.ForFunAndProfit.Applicative
+open FSharpBits.ParserCombinators.ForFunAndProfit.Monad
 open Parser
 open ParseResult
 open Xunit
 open Swensen.Unquote
 
 
-let lift2 (f: 'f -> 'a -> 'b) : 'f Parser -> 'a Parser -> 'b Parser =
-    fun a b -> f <!> a <*> b
+let lift2 (f: 'f -> 'a -> 'b) : 'f Parser -> 'a Parser -> 'b Parser = fun a b -> f <!> a <*> b
 
-let lift3 (f: 'f -> 'a -> 'b -> 'c) : 'f Parser -> 'a Parser -> 'b Parser -> 'c Parser=
-    fun a b c -> f <!> a <*> b <*> c
+let lift3 (f: 'f -> 'a -> 'b -> 'c) : 'f Parser -> 'a Parser -> 'b Parser -> 'c Parser = fun a b c -> f <!> a <*> b <*> c
 
-let parse3Digits' =
-    lift3 combines3Digits parseDigit parseDigit parseDigit
+let parse3Digits' = lift3 combines3Digits parseDigit parseDigit parseDigit
 
 [<Fact>]
 let ``with lift3`` () =
-    test <@ run parse3Digits' "123ZZZ" = Success (123, "ZZZ")@>
+    test <@ run parse3Digits' "123ZZZ" = Success(123, "ZZZ") @>
+
+type Parsed = One | Two | Three | Space
+
+
+let rec sequence (parsers: ('a Parser) list) : ('a list) Parser =
+    let cons (x: 'a) (xs: 'a list) : 'a list = x :: xs
+    let consP: Parser<'a> -> Parser<'a list> -> Parser<'a list> = lift2 cons
+
+    if parsers |> List.isEmpty
+    then returnP []
+    else
+        consP parsers.Head (sequence parsers.Tail)
+
+
+[<Fact>]
+let ``list of parsers`` () =
+
+    let parser (expecting: string) emitting =
+        Parser (fun input ->
+            if input[0..expecting.Length-1] = expecting
+            then Success (emitting, input[expecting.Length..])
+            else Failure $"Expecting '{expecting}'")
+
+    let parseSpace = parser " " Space
+    let parseOne = parser "one" One
+    let parseTwo = parser "two" Two
+    let parseThree = parser "three" Three
+
+
+    let parseList = sequence [parseOne; parseSpace; parseTwo; parseSpace; parseThree]
+
+    test <@ run parseList "one two three" = Success ([ One; Space; Two; Space; Three ], "") @>
