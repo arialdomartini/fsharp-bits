e19a4e1973c1aa076c4ed17cf05ea90322b71d77
Arialdo Martini <arialdo.martini@gmail.com>
2025-09-03 09:38:59 +0200

    Catamorphism: part I, a simple recursive type

---
+++ fsharp-bits/ForFunAndProfit/Catamorphism/RecursiveType.fs
@@ -0,0 +1,23 @@
+module FSharpBits.ForFunAndProfit.Catamorphism.RecursiveType
+
+type Book = { title: string; price: decimal }
+
+
+type ChocolateType =
+    | Black
+    | Milk
+    | SeventyPercent
+
+type Chocolate = { chocolateType: ChocolateType; price: decimal }
+
+type WrappingPaperStyle =
+    | HappyBirthday
+    | HappyHolidays
+    | SolidColor
+
+type Gift =
+    | Book of Book
+    | Chocolate of Chocolate
+    | Wrapped of gift: Gift * wrappingPaperStyle: WrappingPaperStyle
+    | Boxed of Gift
+    | WithACard of gift: Gift * message: string
