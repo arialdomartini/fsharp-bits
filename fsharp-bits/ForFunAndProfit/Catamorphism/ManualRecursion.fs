dee48fa20438097dad54b41660f57afd567d1f3b
Arialdo Martini <arialdo.martini@gmail.com>
2025-09-11 15:54:26 +0200

    Again manual recursion, showing duplication

--- fsharp-bits/ForFunAndProfit/Catamorphism/ManualRecursion.fs
+++ fsharp-bits/ForFunAndProfit/Catamorphism/ManualRecursion.fs
@@ -21,3 +21,19 @@ let ``description of sample values`` () =
     test <@ description book = "Book titled 'Wolf Hall'" @>
     test <@ description gift1 = "Book titled 'Wolf Hall' wrapped in HappyBirthday paper with a card saying Happy Birthday" @>
     test <@ description gift2 = "SeventyPercent chocolate in a box wrapped in HappyHolidays paper" @>
+
+let rec totalCost (gift: Gift) =
+    match gift with
+    | Book book -> book.price
+    | Chocolate chocolate -> chocolate.price
+    | Wrapped(innerGift, _) -> 0.5m + (totalCost innerGift)
+    | Boxed innerGift -> 1.0m + (totalCost innerGift)
+    | WithACard(innerGift, _) -> 2.0m + (totalCost innerGift)
+
+
+[<Fact>]
+let ``total cost of sample values`` () =
+    test <@ totalCost chocolate = 5m @>
+    test <@ totalCost book = 20m @>
+    test <@ totalCost gift1 = 22.5m @>
+    test <@ totalCost gift2 = 6.5m @>
