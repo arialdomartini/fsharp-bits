10e266952f580f789b488a0f19ec785d664ac0c9
Arialdo Martini <arialdo.martini@gmail.com>
2025-09-12 15:04:35 +0200

    Largest file

--- fsharp-bits/ForFunAndProfit/Catamorphism/FileSystem/CataUsage.fs
+++ fsharp-bits/ForFunAndProfit/Catamorphism/FileSystem/CataUsage.fs
@@ -14,5 +14,63 @@ let totalSize (item: FileSystemItem) =
     cataFS fFile fDir item
 
 [<Fact>]
-let ``total size`` () =
-    test <@ totalSize root = 31 @>
+let ``total size`` () = test <@ totalSize root = 31 @>
+
+
+type Microsoft.FSharp.Collections.List<'a> with
+    static member optionMaxBy(f: 'a -> 'b) xs =
+        match xs with
+        | [] -> None
+        | _ -> Some (List.maxBy f xs)
+
+let largestFile (item: FileSystemItem) =
+    let ignoreEmptyDirectories = List.choose id
+
+    let fFile = Some
+
+    let fDir (_: string, _: int, largestFiles: File option list) =
+        match largestFiles with
+        | [] -> None
+        | largestFiles ->
+            largestFiles
+            |> ignoreEmptyDirectories
+            |> List.optionMaxBy _.fileSize
+
+    cataFS fFile fDir item
+
+[<Fact>]
+let ``largestFile `` () =
+
+    let empty = Directory { Directory.dirSize =0; name = "empty"; subItems = [] }
+    let e = Directory { Directory.dirSize =0; name = "empty"; subItems = [ empty ] }
+
+    test <@ largestFile src = Some { name = "build.bat"; fileSize = 3 } @>
+    test <@ largestFile e = None @>
+
+
+let largestFile' (item: FileSystemItem) =
+    let fFile = Some
+
+    let ifNone defaultValue fileOpt =
+        match fileOpt with
+        | Some size -> size
+        | None -> defaultValue
+
+    let fileSize fileOpt =
+        fileOpt
+        |> Option.map _.fileSize
+        |> ifNone 0
+
+    let fDir (_: string, _: int, largestFiles: File option list) =
+        match largestFiles with
+        | [] -> None
+        | largestFiles -> largestFiles |> List.maxBy fileSize
+
+    cataFS fFile fDir item
+
+[<Fact>]
+let ``largestFile size'`` () =
+    let empty = Directory { Directory.dirSize =0; name = "empty"; subItems = [] }
+    let e = Directory { Directory.dirSize =0; name = "empty"; subItems = [ empty ] }
+    test <@ largestFile' root = Some { name = "build.bat"; fileSize = 3 } @>
+    test <@ largestFile e = None @>
