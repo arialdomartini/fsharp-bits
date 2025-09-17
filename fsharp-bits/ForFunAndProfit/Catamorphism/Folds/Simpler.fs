f8fc915cab6451158894e55414c63607763b9b5e
Arialdo Martini <arialdo.martini@gmail.com>
2025-09-17 12:13:22 +0200

    Cata

--- fsharp-bits/ForFunAndProfit/Catamorphism/Folds/Simpler.fs
+++ fsharp-bits/ForFunAndProfit/Catamorphism/Folds/Simpler.fs
@@ -27,6 +27,50 @@ type SampleValues() =
             let innerDescription = descriptionRecursion innerValue
             $"WithAttribute({attribute}, {innerDescription})"
 
+    let rec descriptionQuasiCata (value: Value) =
+
+        let fFinal s = $"Final({s})"
+        let fRecursive innerDescription = $"Recursive({innerDescription})"
+        let fWithAttribute innerDescription attribute = $"WithAttribute({attribute}, {innerDescription})"
+
+        match value with
+        | Final s -> fFinal s
+        | Recursive innerValue ->
+            let innerDescription = descriptionQuasiCata innerValue
+            fRecursive innerDescription
+        | WithAttribute(attribute, innerValue) ->
+            let innerDescription = descriptionQuasiCata innerValue
+            fWithAttribute innerDescription attribute
+
+    let rec cata fFinal fRecursive fWithAttribute (value: Value) =
+        let recurse = cata fFinal fRecursive fWithAttribute
+
+        match value with
+        | Final s -> fFinal s
+        | Recursive innerValue ->
+            let innerDescription = recurse innerValue
+            fRecursive innerDescription
+        | WithAttribute(attribute, innerValue) ->
+            let innerDescription = recurse innerValue
+            fWithAttribute innerDescription attribute
+
+    let descriptionCata (value: Value) =
+
+        let fFinal s = $"Final({s})"
+        let fRecursive innerDescription = $"Recursive({innerDescription})"
+        let fWithAttribute innerDescription attribute = $"WithAttribute({attribute}, {innerDescription})"
+
+        cata fFinal fRecursive fWithAttribute value
+
+
     [<Fact>]
     member this.``description of sample values with plain recursion`` () =
         test <@ descriptionRecursion withAttribute = expectedDescription @>
+
+    [<Fact>]
+    member this.``description of sample values with quasi-catamorphism`` () =
+        test <@ descriptionQuasiCata withAttribute = expectedDescription @>
+
+    [<Fact>]
+    member this.``description of sample values with catamorphism`` () =
+        test <@ descriptionCata withAttribute = expectedDescription @>
