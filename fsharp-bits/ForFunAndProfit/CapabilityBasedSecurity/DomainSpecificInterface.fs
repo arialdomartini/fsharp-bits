d09c925c04357cd01648387242cf1fd8ebbb7724
Arialdo Martini <arialdo.martini@gmail.com>
2025-09-19 18:32:05 +0200

    Domain centric interface

--- fsharp-bits/ForFunAndProfit/CapabilityBasedSecurity/DomainSpecificInterface.fs
+++ fsharp-bits/ForFunAndProfit/CapabilityBasedSecurity/DomainSpecificInterface.fs
@@ -1,4 +1,4 @@
-module FSharpBits.ForFunAndProfit.CapabilityBasedSecurity.KeyValueInterface
+module FSharpBits.ForFunAndProfit.CapabilityBasedSecurity.DomainSpecificInterface
 
 open System.IO
 
@@ -14,22 +14,26 @@ let private fromJson (content: string) =
 
 let private toJson (content: 'a) = JsonSerializer.Serialize(content)
 
-
-let setConfigurationValue key value =
+let private setUrl value =
     let configurationFileName = "shared-configuration-file.conf"
-
     let configuration = File.ReadAllText(configurationFileName) |> fromJson
 
-    let newConfiguration =
-        match key with
-        | "username" -> { configuration with username = value }
-        | "password" -> { configuration with password = value }
-        | "url" -> { configuration with url = value }
-        | _ -> configuration
+    let newConfiguration = { configuration with url = value }
 
     File.WriteAllText(configurationFileName, newConfiguration |> toJson)
+
+type Configs =
+    { setUsername: string -> unit
+      setPassword: string -> unit
+      setUrl : string -> unit }
+
+let configs =
+    { setUsername = failwith "Not yet implemented"
+      setPassword = failwith "Not yet implemented"
+      setUrl = setUrl }
+
 
 
 module Client =
 
-    setConfigurationValue "url" "new-value"
+    configs.setUrl "new-value"
